35e695322729514c7a24bdd1435c8c5a82c644de
Georgi Kunchev <gvkunchev@gmail.com>
2023-05-25 16:15:26 +0300

    Cosmetic error message change

--- django/shrubbery/homeworks/tasks.py
+++ django/shrubbery/homeworks/tasks.py
@@ -1,97 +1,15 @@
-import os
-import json
-import shutil
-import subprocess
-import tempfile
-
-from django.conf import settings
 from celery import shared_task
 
-
-from .models import Homework
-from homeworksolutions.models import HomeworkSolution
-
-
-class TestsRunner:
-    """Runner for homework tests."""
-
-    SANDBOX_MATRIX = '/var/shrubbery/sandbox/sandbox-origin'
-    SANDBOX_TMP_COPY = '/var/shrubbery/sandbox/sandbox{}'
-    TEST_RUNNER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_runner.py')
-    TIMEOUT = 2 # seconds
-
-    def __init__(self, homework_pk):
-        '''Initializator.'''
-        self._homework_pk = homework_pk
-        self._homework = None
-        self._solutions = None
-        self._work_dir = None
-        self._prd = os.environ.get('SHRUBBERY_ENV') == 'prd'
-        self._prepare_data()
-        self._prepare_env()
-        self._execute()
-        self._cleanup()
-    
-    def _prepare_data(self):
-        """Prepare the data from database."""
-        self._homework = Homework.objects.get(pk=self._homework_pk)
-        self._solutions = HomeworkSolution.objects.filter(homework=self._homework)
-    
-    def _prepare_env(self):
-        """Prepare the environment."""
-        if self._prd:
-            # Create sandbox filesystem
-            self._work_dir = self.SANDBOX_TMP_COPY.format(self._homework_pk)
-            os.system(f'cp -r {self.SANDBOX_MATRIX} {self._work_dir}')
-            # Copy the test runner
-            shutil.copyfile(self.TEST_RUNNER, os.path.join(self._work_dir, 'tmp/test_runner.py'))
-            # Copy the test itself
-            with open(os.path.join(self._work_dir, 'tmp/test.py'), 'w') as f:
-                f.write(self._homework.full_test)
-        else:
-            # Create a temp directory
-            self._work_dir = os.path.join(tempfile.gettempdir(), f'homework{self._homework_pk}')
-            os.mkdir(self._work_dir)
-            # Copy the test runner
-            shutil.copyfile(self.TEST_RUNNER, os.path.join(self._work_dir, 'test_runner.py'))
-            # Copy the test itself
-            with open(os.path.join(self._work_dir, 'test.py'), 'w') as f:
-                f.write(self._homework.full_test)
-    
-    def _execute(self):
-        """Execute all tests."""
-        for solution in self._solutions:
-            source_code = os.path.join(settings.MEDIA_ROOT, solution.content.path)
-            if self._prd:
-                # Copy the solution
-                shutil.copy(source_code, os.path.join(self._work_dir, 'tmp/solution.py'))
-                # Build the command
-                command = (f'env -i /usr/sbin/chroot {self._work_dir} '
-                            '/usr/bin/python3.10 /tmp/test_runner.py /tmp/test.py')
-            else:
-                # Copy the solution
-                shutil.copy(source_code, os.path.join(self._work_dir, 'solution.py'))
-                # Build the command
-                test_runner = os.path.join(self._work_dir, 'test_runner.py')
-                test = os.path.join(self._work_dir, 'test.py')
-                command = (f'python {test_runner} {test}')
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, _ = process.communicate(timeout=self.TIMEOUT)
-            json_result = json.loads(stdout)
-            solution.passed_tests = len(json_result['passed'])
-            solution.failed_tests = len(json_result['failed'])
-            solution.result = json_result['log']
-            solution.save()
-            solution.assign_points()
-
-    def _cleanup(self):
-        """Cleanup temp dirs and release the model."""
-        shutil.rmtree(self._work_dir)
-        self._homework.executing_tests = False
-        self._homework.save()
+from .test_runners import FullTestsRunner, SanityTestsRunner
 
 
 @shared_task()
 def run_homework_tests(homework_pk):
-    """Tun tests for all solutions of a homework.."""
-    TestsRunner(homework_pk)
+    """Run tests for all solutions of a homework.."""
+    FullTestsRunner(homework_pk)
+
+
+@shared_task()
+def run_sanity_test(solution):
+    """Run sanity test for a single solution of a homework.."""
+    return SanityTestsRunner(solution).get_results()
---
+++ django/shrubbery/homeworks/test_runners.py
@@ -0,0 +1,141 @@
+import os
+import uuid
+import json
+import shutil
+import subprocess
+import tempfile
+
+from django.conf import settings
+
+from .models import Homework
+from homeworksolutions.models import HomeworkSolution
+
+
+class TestsRunner:
+    """Tests runner for full and sanity test."""
+
+    SANDBOX_MATRIX = '/var/shrubbery/sandbox/sandbox-origin'
+    SANDBOX_TMP_COPY = '/var/shrubbery/sandbox/sandbox{}'
+    TEST_RUNNER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'test_runner.py')
+    TIMEOUT = 2 # seconds
+    TEST_KEY = None # To be set in child classes
+
+    def __init__(self):
+        """Initializator."""
+        self._work_dir = None
+        self._temp_id = uuid.uuid4()
+        self._prd = os.environ.get('SHRUBBERY_ENV') == 'prd'
+        self._prepare_data()
+        self._prepare_env()
+        self._execute()
+        self._cleanup()
+
+    def _prepare_env(self):
+        """Prepare the environment."""
+        if self._prd:
+            # Create sandbox filesystem
+            self._work_dir = self.SANDBOX_TMP_COPY.format(self._temp_id)
+            os.system(f'cp -r {self.SANDBOX_MATRIX} {self._work_dir}')
+            # Copy the test runner
+            shutil.copyfile(self.TEST_RUNNER, os.path.join(self._work_dir, 'tmp/test_runner.py'))
+            # Copy the test itself
+            with open(os.path.join(self._work_dir, 'tmp/test.py'), 'w') as f:
+                f.write(getattr(self._homework, self.TEST_KEY))
+        else:
+            # Create a temp directory
+            self._work_dir = os.path.join(tempfile.gettempdir(), f'homework{self._temp_id}')
+            os.mkdir(self._work_dir)
+            # Copy the test runner
+            shutil.copyfile(self.TEST_RUNNER, os.path.join(self._work_dir, 'test_runner.py'))
+            # Copy the test itself
+            with open(os.path.join(self._work_dir, 'test.py'), 'w') as f:
+                f.write(getattr(self._homework, self.TEST_KEY))
+    
+    def _prepare_command(self, solution):
+        """Prepare the solution in place and the command to run the test."""
+        source_code = os.path.join(settings.MEDIA_ROOT, solution.content.path)
+        if self._prd:
+            # Copy the solution
+            shutil.copy(source_code, os.path.join(self._work_dir, 'tmp/solution.py'))
+            # Build the command
+            return (f'env -i /usr/sbin/chroot {self._work_dir} '
+                     '/usr/bin/python3.10 /tmp/test_runner.py /tmp/test.py')
+        else:
+            # Copy the solution
+            shutil.copy(source_code, os.path.join(self._work_dir, 'solution.py'))
+            # Build the command
+            test_runner = os.path.join(self._work_dir, 'test_runner.py')
+            test = os.path.join(self._work_dir, 'test.py')
+            return f'python {test_runner} {test}'
+
+    def _cleanup(self):
+        """Cleanup temp dirs and release the model."""
+        shutil.rmtree(self._work_dir)
+
+
+class FullTestsRunner(TestsRunner):
+    """Runner for homework full tests."""
+
+    TEST_KEY = 'full_test'
+
+    def __init__(self, homework_pk):
+        '''Initializator.'''
+        self._homework_pk = homework_pk
+        self._homework = None
+        self._solutions = None
+        super(FullTestsRunner, self).__init__()
+    
+    def _prepare_data(self):
+        """Prepare the data from database."""
+        self._homework = Homework.objects.get(pk=self._homework_pk)
+        self._solutions = HomeworkSolution.objects.filter(homework=self._homework)
+    
+    def _execute(self):
+        """Execute all tests."""
+        for solution in self._solutions:
+            command = self._prepare_command(solution)
+            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, _ = process.communicate(timeout=self.TIMEOUT)
+            json_result = json.loads(stdout)
+            solution.passed_tests = len(json_result['passed'])
+            solution.failed_tests = len(json_result['failed'])
+            solution.result = json_result['log']
+            solution.save()
+            solution.assign_points()
+
+    def _cleanup(self):
+        """Cleanup temp dirs and release the model."""
+        super(FullTestsRunner, self)._cleanup()
+        self._homework.executing_tests = False
+        self._homework.save()
+
+
+class SanityTestsRunner(TestsRunner):
+    """Runner for homework sanity tests."""
+
+    TEST_KEY = 'sanity_test'
+
+    def __init__(self, solution):
+        '''Initializator.'''
+        self._solution = solution
+        self._json_result = None
+        super(SanityTestsRunner, self).__init__()
+    
+    def _prepare_data(self):
+        """Prepare the data from database."""
+        self._homework = self._solution.homework
+    
+    def _execute(self):
+        """Execute all tests."""
+        command = self._prepare_command(self._solution)
+        process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, _ = process.communicate(timeout=self.TIMEOUT)
+        self._json_result = json.loads(stdout)
+
+    def _cleanup(self):
+        """Cleanup temp dirs and release the model."""
+        super(SanityTestsRunner, self)._cleanup()
+    
+    def get_results(self):
+        """Get test results."""
+        return self._json_result
