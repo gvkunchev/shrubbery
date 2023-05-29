7a185c7e5636c9c2c1d462bbbedcb748ce3e476d
Georgi Kunchev <gvkunchev@gmail.com>
2023-05-29 09:56:42 +0300

    Add word-wrap for diff

--- django/shrubbery/shrubbery/static/js/controls.js
+++ django/shrubbery/shrubbery/static/js/controls.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
         var inputs = $(this).parents('form').find('input[type=checkbox');
         inputs.click()
     })
+
+    $('[data-trigger="word-wrap"]').bind('change', function(){
+        if($(this).is(':checked')){
+            $(this).parents('.code-diff-container').addClass('word-wrap-diff');
+        }
+        else{
+            $(this).parents('.code-diff-container').removeClass('word-wrap-diff');
+        }
+    })
 })
