ddf174934e9059885623f977b35dd86350d664c3
Georgi Kunchev <gvkunchev@gmail.com>
2023-07-03 16:12:13 +0300

    Set real email sender

--- django/shrubbery/shrubbery/email.py
+++ django/shrubbery/shrubbery/email.py
@@ -1,17 +1,28 @@
 import os
-import yagmail
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Emailer:
     """Email."""
 
     SENDER = os.environ.get('GMAIL_USER')
+    SENDER_ALIAS = 'noreply@py-fmi.org'
     PASSWORD = os.environ.get('GMAIL_PASS')
 
     def __init__(self):
         """Initializator."""
-        self._yag = yagmail.SMTP(self.SENDER, self.PASSWORD)
+        self._smtp = smtplib.SMTP('smtp.gmail.com', 587)
+        self._smtp.starttls()
+        self._smtp.login(self.SENDER, self.PASSWORD)
 
     def send_email(self, recipients, subject, body):
         """Send email."""
-        self._yag.send(bcc=recipients, subject=subject, contents=body)
+        message = MIMEMultipart()
+        message['From'] = self.SENDER_ALIAS
+        message['To'] = self.SENDER_ALIAS
+        message['Bcc'] = ','.join(recipients)
+        message['Subject'] = subject
+        message.attach(MIMEText(body, 'html'))
+        self._smtp.send_message(message)
