46d4ee9746e86294aad8d58ede7a192eda96c8c0
Georgi Kunchev <gvkunchev@gmail.com>
2023-07-26 14:26:27 +0300

    Add forgotten password option

--- django/shrubbery/users/emails.py
+++ django/shrubbery/users/emails.py
@@ -20,3 +20,16 @@ def send_activation_email(request, user):
                     'token': account_activation_token.make_token(user),
                 })
     EMAILER.send_email([user.email], mail_subject, message)
+
+
+def send_new_password_email(request, user):
+    """Send email with link for changing the password to a user."""
+    current_site = get_current_site(request)
+    mail_subject = 'Смени пароалта на своя акаунт за "Python @ ФМИ"'
+    message = render_to_string('emails/new_password_email.html', {
+                    'user': user.full_name,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                    'token': account_activation_token.make_token(user),
+                })
+    EMAILER.send_email([user.email], mail_subject, message)
