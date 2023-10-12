515dcab162c45951b6a2cd3fa3eacaa3e0c31765
Georgi Kunchev <gvkunchev@gmail.com>
2023-10-12 18:32:40 +0300

    Allow students to see their own points summary

--- django/shrubbery/points/urls.py
+++ django/shrubbery/points/urls.py
@@ -8,5 +8,6 @@ app_name = 'points'
 
 urlpatterns = [
     re_path(r'^points$', views.points, name='points'),
+    re_path(r'^my_points$', views.my_points, name='my_points'),
     re_path(r'^scoreboard$', views.scoreboard, name='scoreboard'),
 ]
