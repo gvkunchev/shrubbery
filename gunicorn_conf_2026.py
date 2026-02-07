8afdab4f55efd761a4cefe19191ac532fdadb790
Georgi Kunchev <gvkunchev@gmail.com>
2026-02-07 11:43:29 +0200

    Further optimize

--- gunicorn_conf_2026.py
+++ gunicorn_conf_2026.py
@@ -3,18 +3,16 @@
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "shrubbery.wsgi:application"
 # The granularity of Error log outputs
-loglevel = "debug"
+loglevel = "info"
 # The number of worker processes for handling requests
-workers = 2
+workers = 1
+threads = 4
+preload_app=True
 # The socket to bind
 bind = "0.0.0.0:8026"
-# Restart workers when code changes (development only!)
-reload = True
 # Write access and error info to /var/log
 accesslog = errorlog = "/var/log/gunicorn/dev_2026.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
 pidfile = "/var/run/gunicorn/dev_2026.pid"
-# Daemonize the Gunicorn process (detach & enter background)
-daemon = True
